294a885e3d440066490b6d22eea3b87247eb323b
Roland <roland.bird@gmail.com>
2025-11-14 07:33:12 +1300

    Create app.py

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, redirect, render_template_string
+from onelogin.saml2.auth import OneLogin_Saml2_Auth
+import os
+
+app = Flask(__name__)
+
+def init_saml_auth(req):
+    auth = OneLogin_Saml2_Auth(req, custom_base_path=os.path.join(os.getcwd(), 'saml'))
+    return auth
+
+def prepare_flask_request(request):
+    url_data = request.url.split('?')
+    return {
+        'https': 'on' if request.scheme == 'https' else 'off',
+        'http_host': request.host,
+        'script_name': request.path,
+        'server_port': request.environ.get('SERVER_PORT'),
+        'get_data': request.args.copy(),
+        'post_data': request.form.copy()
+    }
+
+@app.route('/')
+def index():
+    return '/login/Login with SAML</a>'
+
+@app.route('/login/')
+def login():
+    req = prepare_flask_request(request)
+    auth = init_saml_auth(req)
+    return redirect(auth.login())
+
+@app.route('/acs/', methods=['POST'])
+def acs():
+    req = prepare_flask_request(request)
+    auth = init_saml_auth(req)
+    auth.process_response()
+    errors = auth.get_errors()
+    if not errors:
+        attributes = auth.get_attributes()
+        name_id = auth.get_nameid()
+        return render_template_string("""
+            <h1>SAML Response</h1>
+            <p>NameID: {{ name_id }}</p>
+            <pre>{{ attributes }}</pre>
+        """, name_id=name_id, attributes=attributes)
+    else:
+        return f"Errors: {errors}"
+
+@app.route('/metadata/')
+def metadata():
+    req = prepare_flask_request(request)
+    auth = init_saml_auth(req)
+    return auth.get_settings().get_sp_metadata()
+
+if __name__ == '__main__':
+    app.run(debug=True)
